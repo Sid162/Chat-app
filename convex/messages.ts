4c6bffc78abebd8eb344dab74a70a306c7bc7289
Sid162 <65181412+Sid162@users.noreply.github.com>
2024-03-18 16:08:54 -0500

    Add files via upload

---
+++ convex/messages.ts
@@ -0,0 +1,58 @@
+import { query, mutation } from "./_generated/server";
+import { api } from "./_generated/api";
+import { v } from "convex/values";
+
+export const send = mutation({
+  args: { body: v.string(), author: v.string() },
+  handler: async (ctx, args) => {
+    const { body, author } = args;
+    // Send a new message.
+    await ctx.db.insert("messages", { body, author });
+
+    if (/\b@gpt\b/i.test(body) && author !== "ChatGPT") {
+      // Schedule the chat action to run immediately
+      const res = await ctx.scheduler.runAfter(0, api.openai.chat, {
+        messageBody: body,
+      });
+    }
+  },
+});
+
+export const like = mutation({
+  args: { liker: v.string(), messageId: v.id("messages") },
+  handler: async (ctx, args) => {
+    // Save a user's "like" of a particular message
+    await ctx.db.insert("likes", {
+      liker: args.liker,
+      messageId: args.messageId,
+    });
+  },
+});
+
+export const list = query({
+  args: {},
+  handler: async (ctx) => {
+    // Grab the most recent messages.
+    const messages = await ctx.db.query("messages").order("desc").take(100);
+    const messagesWithLikes = await Promise.all(
+      messages.map(async (message) => {
+        // Find the likes for each message
+        const likes = await ctx.db
+          .query("likes")
+          .withIndex("byMessageId", (q) => q.eq("messageId", message._id))
+          .collect();
+        // Join the count of likes with the message data
+        return {
+          ...message,
+          likes: likes.length,
+        };
+      }),
+    );
+    // Reverse the list so that it's in a chronological order.
+    return messagesWithLikes.reverse().map((message) => ({
+      ...message,
+      // Format smileys
+      body: message.body.replaceAll(":)", "😊"),
+    }));
+  },
+});
